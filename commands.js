8646fe54a8094736b81f973ae22cb7ff7660b990
Knight <73390911+KnightBits@users.noreply.github.com>
2024-07-17 16:21:59 +0300

    Update commands.js
    Fixed code + optimization + errors log + updated command's

--- commands.js
+++ commands.js
@@ -1,111 +1,39 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { PermissionsBitField } = require('discord.js');
-const { help } = require('./help');
-const { kick } = require('./kick');
-const { ban } = require('./ban');
-const { timeout } = require('./timeout');
-const { profile } = require('./profile');
-
-const commands = [
-    new SlashCommandBuilder()
-        .setName('help')
-        .setDescription('Helps about all commands of GA BOT X'),
-
-    new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with Pong! and checks the API latency'),
-
-    new SlashCommandBuilder()
-        .setName('profile')
-        .setDescription('Shows the information about a user')
-        .addUserOption(option => option.setName('user').setDescription('Enter username to see user info').setRequired(true)),
-
-    new SlashCommandBuilder()
-        .setName('play')
-        .setDescription('Plays a song'),
-
-    new SlashCommandBuilder()
-        .setName('kick')
-        .setDescription('Kicks a user')
-        .addUserOption(option => option.setName('user').setDescription('Enter the username to kick').setRequired(true))
-        .addStringOption(option => option.setName('reason').setDescription('Enter reason').setRequired(false)),
-
-    new SlashCommandBuilder()
-        .setName('ban')
-        .setDescription('Bans a user')
-        .addUserOption(option => option.setName('user').setDescription('Enter the username to ban').setRequired(true))
-        .addStringOption(option => option.setName('time').setDescription('Ban duration (e.g., 1s = 1 second, 1m = 1 minute, 1h = 1 hour, 1d = 1 day, 1y = 1 year)').setRequired(false))
-        .addStringOption(option => option.setName('reason').setDescription('Enter reason').setRequired(false)),
-
-    new SlashCommandBuilder()
-        .setName('stop')
-        .setDescription('Stops song'),
-
-    new SlashCommandBuilder()
-        .setName('pause')
-        .setDescription('Pauses song'),
-
-    new SlashCommandBuilder()
-        .setName('resume')
-        .setDescription('Resumes song'),
-    
-    new SlashCommandBuilder()
-        .setName('timeout')
-        .setDescription('Timeouts a user')
-        .addUserOption(option => option.setName('user').setDescription('Enter the username to timeout').setRequired(true))
-        .addStringOption(option => option.setName('time').setDescription('Timeout duration (e.g., 1s = 1 second, 1m = 1 minute, 1h = 1 hour, 1d = 1 day, 1mo = 1 month)').setRequired(true))
-        .addStringOption(option => option.setName('reason').setDescription('Enter reason').setRequired(false)),
-];
-
 const handleCommand = async interaction => {
-
     const member = interaction.member;
     const ownerID = interaction.guild.ownerId;
 
-    if (interaction.commandName === 'play') {
-        await interaction.reply({ content: 'It is in testing mode', ephemeral: true });
-    } 
-    
-    else if (interaction.commandName === 'help') {
-        await help(interaction);
-    } 
-    
-    else if (interaction.commandName === 'profile') {
-        await profile(interaction);
-    } 
-    
-    else if (interaction.commandName === 'ping') {
-        const sent = await interaction.reply({ content: 'Pong!', fetchReply: true, ephemeral: true });
-        const latency = sent.createdTimestamp - interaction.createdTimestamp;
-        await interaction.editReply({ content: `Pong!\nLatency is ${latency}ms.\nAPI Latency is ${Math.round(interaction.client.ws.ping)}ms`, ephemeral: true });
-    } 
-    
-    else if (interaction.commandName === 'kick') {
-        if (member.permissions.has(PermissionsBitField.Flags.KickMembers) || member.permissions.has(PermissionsBitField.Flags.Administrator) || member.id === ownerID) {
-            await kick(interaction);
-        } else {
-            await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+    try {
+        if (interaction.commandName === 'play') {
+            await interaction.reply({ content: 'It is in testing mode', ephemeral: true });
+        } else if (interaction.commandName === 'help') {
+            await help(interaction);
+        } else if (interaction.commandName === 'profile') {
+            await profile(interaction);
+        } else if (interaction.commandName === 'ping') {
+            const sent = await interaction.reply({ content: 'Pong!', fetchReply: true, ephemeral: true });
+            const latency = sent.createdTimestamp - interaction.createdTimestamp;
+            await interaction.editReply({ content: `Pong!\nLatency is ${latency}ms.\nAPI Latency is ${Math.round(interaction.client.ws.ping)}ms`, ephemeral: true });
+        } else if (interaction.commandName === 'kick') {
+            if (member.permissions.has(PermissionsBitField.Flags.KickMembers) || member.permissions.has(PermissionsBitField.Flags.Administrator) || member.id === ownerID) {
+                await kick(interaction);
+            } else {
+                await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+            }
+        } else if (interaction.commandName === 'ban') {
+            if (member.permissions.has(PermissionsBitField.Flags.BanMembers) || member.permissions.has(PermissionsBitField.Flags.Administrator) || member.id === ownerID) {
+                await ban(interaction);
+            } else {
+                await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+            }
+        } else if (interaction.commandName === 'timeout') {
+            if (member.permissions.has(PermissionsBitField.Flags.ModerateMembers) || member.permissions.has(PermissionsBitField.Flags.Administrator) || member.id === ownerID) {
+                await timeout(interaction);
+            } else {
+                await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+            }
         }
-    } 
-    
-    else if (interaction.commandName === 'ban') {
-        if (member.permissions.has(PermissionsBitField.Flags.BanMembers) || member.permissions.has(PermissionsBitField.Flags.Administrator) || member.id === ownerID) {
-            await ban(interaction);
-        } else {
-            await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
-        }
-    } 
-    
-    else if (interaction.commandName === 'timeout') {
-        if (member.permissions.has(PermissionsBitField.Flags.ModerateMembers) || member.permissions.has(PermissionsBitField.Flags.Administrator) || member.id === ownerID) {
-            await timeout(interaction);
-        } else {
-            await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
-        }
+    } catch (error) {
+        console.error(`Error handling command ${interaction.commandName}:`, error);
+        await interaction.reply({ content: 'An error occurred while executing the command.', ephemeral: true });
     }
-};
-
-module.exports = {
-    commands,
-    handleCommand
 };
